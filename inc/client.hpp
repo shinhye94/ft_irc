6e022e8dfe769dc0e5b4e5c35c2fbf1452b01495
mozaBit <bamhammedmetehri@gmail.com>
2025-01-15 16:12:10 +0100

    first commit

---
+++ inc/client.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bmetehri <bmetehri@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/15 15:09:24 by bmetehri          #+#    #+#             */
+/*   Updated: 2025/01/15 15:12:22 by bmetehri         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef __CLIENT_HPP__
+# define __CLIENT_HPP__
+
+
+#include <iostream>
+#include <string>
+
+class Client {
+	private:
+		std::string	_username;
+		std::string	_nickname;
+	public:
+};
+
+#endif`
